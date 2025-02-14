3eedab17881923cbcfa9a3a7820a44217ca9223e
Mohamed Hameed <mohammedhameedhassan@gmail.com>
2025-02-14 10:14:08 +0200

    second commit

--- src/Components/Checkout/Checkout.jsx
+++ src/Components/Checkout/Checkout.jsx
@@ -18,7 +18,7 @@ export default function Checkout() {
   async function Checkout(shippingAddress){
     try{
       setloading(true)
-      let {data} =await axios.post(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cart.cartId}?url=https://fresh-cart-ecommerce-flax.vercel.app/allorders`,{
+      let {data} =await axios.post(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cart.cartId}?url=https://fresh-cart-ecommerce-flax.vercel.app/`,{
         shippingAddress
       },{
         headers:{
@@ -27,6 +27,10 @@ export default function Checkout() {
       })
     console.log(data);
     toast.success(data.status)
+location.href=data.session.url;
+  
+   
+    
     
 
     
